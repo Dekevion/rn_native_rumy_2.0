c9c9405269cbc067c4dd14df98ddb8ab2c0680fe
Dekevion Wade <Dekevionwade@yahoo.com>
2019-12-26 14:07:10 -0600

    progress2

--- Rumy/screens/MagicScreen.js
+++ Rumy/screens/MagicScreen.js
@@ -1,21 +1,142 @@
 import React, {Component} from 'react';
-import {View, TextInput, Text, StyleSheet, Image,
+import {
+    View, TextInput, Text, StyleSheet, Image,
     TouchableWithoutFeedback, SafeAreaView, Keyboard,
-    TouchableOpacity, KeyboardAvoidingView,Dimensions} from "react-native";
+    TouchableOpacity, KeyboardAvoidingView, Dimensions, PermissionsAndroid,
+Button} from "react-native";
 import {LinearGradient} from "expo-linear-gradient";
 
-class MagicScreen extends Component {
-    render() {
-        function getLocation () {
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-        }, error => console.log(error))
-
+// class MagicScreen extends Component {
+//     render() {
+//         try{
+//              this.getLocation = () => {
+//                 navigator.geolocation.getCurrentPosition(position => {
+//                     console.log(position);
+//                 }, error => console.log(error));
+//                 const granted = PermissionsAndroid.request(
+//                     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//                     {
+//                         title: 'Access User Location Permission',
+//                         message: 'To Continue, turn on device location.',
+//                         buttonNeutral: 'Ask Me Later',
+//                         buttonNegative: 'Cancel',
+//                         buttonPositive: 'OK'
+//                     },
+//                 );
+//                 if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//                     console.log('You can use the map');
+//                 } else {
+//                     console.log('Permission Denied')
+//                 }
+//             }
+//         } catch (e) {
+//             console.warn(e);
+//         }
+//
+//
+//
+//         return (
+//             <LinearGradient colors={['#0f2027', '#203a43', '#2c5364']} style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
+//
+//             <View style={styles.container}>
+//
+//                 <Text style={styles.tex}>
+//                     Where the Magic Happens!
+//                 </Text>
+//
+//
+//                 <TouchableOpacity activeOpacity={0.8}>
+//                     <View style={styles.moveButton}>
+//                         <Text style={styles.editButton} onPress= {() => getLocation()}>Fetch Location</Text>
+//                     </View>
+//                 </TouchableOpacity>
+//
+//
+//             </View>
+//             </LinearGradient>
+//         );
+//     }
+// }
+// const MagicScreen = (props) => {
+//     try{
+//       function getLocation () {
+//             navigator.geolocation.getCurrentPosition(position => {
+//                 console.log(position);
+//             }, error => console.log(error));
+//             const granted = PermissionsAndroid.request(
+//                 PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+//                 {
+//                     title: 'Access User Location Permission',
+//                     message: 'To Continue, turn on device location.',
+//                     buttonNeutral: 'Ask Me Later',
+//                     buttonNegative: 'Cancel',
+//                     buttonPositive: 'OK'
+//                 },
+//             );
+//             if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+//                 console.log('You can use the map');
+//             } else {
+//                 console.log('Permission Denied')
+//             }
+//         }
+//     } catch (e) {
+//         console.warn(e);
+//     }
+//     return (
+//         <LinearGradient colors={['#0f2027', '#203a43', '#2c5364']}
+//                         style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
+//
+//             <View style={styles.container}>
+//
+//                 <Text style={styles.tex}>
+//                     Where the Magic Happens!
+//                 </Text>
+//
+//
+//                 <TouchableOpacity activeOpacity={0.8}>
+//                     <View style={styles.moveButton}>
+//                         <Text style={styles.editButton} onPress={() => getLocation()}>Fetch Location</Text>
+//                     </View>
+//                 </TouchableOpacity>
+//
+//
+//             </View>
+//         </LinearGradient>
+//     )
+// };
+const MagicScreen = () => {
+    function locationPlease () {
+        try{
+             {
+            navigator.geolocation.getCurrentPosition(position => {
+                console.log(position);
+            }, error => console.log(error));
+            const granted = PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Access User Location Permission',
+                    message: 'To Continue, turn on device location.',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK'
+                },
+            );
+            // if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //     console.log('You can use the map');
+            // } else {
+            //     console.log('Permission Denied')
+            // }
         }
-
-
-        return (
-            <LinearGradient colors={['#0f2027', '#203a43', '#2c5364']} style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
+    } catch (e) {
+        console.warn(e);
+    }
+    }
+    return (
+        // <View style={styles.button}>
+        //     <Button title="Please" onPress={() => locationPlease()}></Button>
+        // </View>
+        <LinearGradient colors={['#0f2027', '#203a43', '#2c5364']}
+                        style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
 
             <View style={styles.container}>
 
@@ -26,37 +147,40 @@ class MagicScreen extends Component {
 
                 <TouchableOpacity activeOpacity={0.8}>
                     <View style={styles.moveButton}>
-                        <Text style={styles.editButton} onPress={() => getLocation()}>Fetch Location</Text>
+                        <Text style={styles.editButton} onPress={() => locationPlease()}>Fetch Location</Text>
                     </View>
                 </TouchableOpacity>
 
 
             </View>
-            </LinearGradient>
-        );
-    }
-}
+        </LinearGradient>
+    )
+};
 const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            // backgroundColor: '#212121',
-            alignItems:'center',
-            justifyContent: 'center',
-        },
-        tex: {
-            fontFamily: 'LilitaOne-Regular',
-            color: "#ffffff",
-            marginVertical: 50
-        },
+    container: {
+        flex: 1,
+        // backgroundColor: '#212121',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    tex: {
+        fontFamily: 'LilitaOne-Regular',
+        color: "#ffffff",
+        marginVertical: 50
+    },
     editButton: {
         backgroundColor: '#Fffafa',
         height: 30,
         borderRadius: 50,
         textAlign: 'center',
         fontSize: 20,
-        justifyContent:'center',
+        justifyContent: 'center',
         padding: 1
     },
-
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
 export default MagicScreen;
