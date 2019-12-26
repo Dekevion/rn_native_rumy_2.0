48a01c23fa996f3a7f41704fdd93b9f6e2f0c60b
Dekevion Wade <Dekevionwade@yahoo.com>
2019-12-26 12:15:22 -0600

    please

--- Rumy/screens/MagicScreen.js
+++ Rumy/screens/MagicScreen.js
@@ -6,14 +6,31 @@ import {LinearGradient} from "expo-linear-gradient";
 
 class MagicScreen extends Component {
     render() {
+        function getLocation () {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+        }, error => console.log(error))
+
+        }
+
+
         return (
             <LinearGradient colors={['#0f2027', '#203a43', '#2c5364']} style={{flex: 1, alignItems: 'center', justifyContent: "center"}}>
 
             <View style={styles.container}>
 
-                <Text>
+                <Text style={styles.tex}>
                     Where the Magic Happens!
                 </Text>
+
+
+                <TouchableOpacity activeOpacity={0.8}>
+                    <View style={styles.moveButton}>
+                        <Text style={styles.editButton} onPress={() => getLocation()}>Fetch Location</Text>
+                    </View>
+                </TouchableOpacity>
+
+
             </View>
             </LinearGradient>
         );
@@ -26,6 +43,20 @@ const styles = StyleSheet.create({
             alignItems:'center',
             justifyContent: 'center',
         },
+        tex: {
+            fontFamily: 'LilitaOne-Regular',
+            color: "#ffffff",
+            marginVertical: 50
+        },
+    editButton: {
+        backgroundColor: '#Fffafa',
+        height: 30,
+        borderRadius: 50,
+        textAlign: 'center',
+        fontSize: 20,
+        justifyContent:'center',
+        padding: 1
+    },
 
 });
 export default MagicScreen;
